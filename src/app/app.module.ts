7bec441fa61fde04f90d96c1dd0c4ef1d9975bac
Rh Dev <rhezannalbarracin.rc@gmail.com>
2017-11-07 19:10:56 +0800

    Add Dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+/* App Root */
 import { AppComponent } from './app.component';
+
+/* Feature Modules */
 import { LoginComponent } from './login/login.component';
+
+/*Routing Modules */
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+
+    AppRoutingModule,
+
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
