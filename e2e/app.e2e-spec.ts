e11f3752c8a3a892d6c20ffe14766e1d2b230a32
Rh Dev <rhezannalbarracin.rc@gmail.com>
2017-11-07 16:44:16 +0800

    Initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { DreamAppPage } from './app.po';
+
+describe('dream-app App', function() {
+  let page: DreamAppPage;
+
+  beforeEach(() => {
+    page = new DreamAppPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
